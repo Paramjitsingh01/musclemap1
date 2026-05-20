79a1e3769ddc2e9a04e2633c6317d01398f9cc6d
Paramjitsingh01 <161873052+Paramjitsingh01@users.noreply.github.com>
2026-05-20 15:39:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ try:
 
     genai.configure(api_key=GEMINI_API_KEY)
 
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 except Exception as e:
     st.error("Gemini API Setup Error")
